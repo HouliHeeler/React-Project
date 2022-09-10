c0ef28e9056c92192deefe1d375dc16951ce6338
HouliHeeler <luke_houlihan@hotmail.com>
2022-09-09 17:17:47 -0700

    react jsx/reactDOM

---
+++ index.js
@@ -0,0 +1,13 @@
+function Main() {
+    return (
+        <ul>
+            <li>Eggs</li>
+            <li>Milk</li>
+            <li>Bread</li>
+        </ul>
+    )
+}
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById("root"))
