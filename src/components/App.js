135ed83aee72b00c3078329cda2f156fe590e7f7
HouliHeeler <luke_houlihan@hotmail.com>
2022-09-09 18:59:09 -0700

    custom React components

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import '../../src/App.css';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <h1>React Project</h1>
+        <ul>
+          <li>Eggs</li>
+          <li>Milk</li>
+          <li>Bread</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Navbar() {
+    return (
+        <div>
+            <h1>Expatriate</h1>
+            <ul>
+                <li>Link 1</li>
+                <li>Link 2</li>
+                <li>Link 3</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
